0a274de52baf3d83ebb0145de1e1912c5bd51923
Mantongash <you@example.com>
2020-02-23 16:27:50 +0300

    Added neighborhood app to the settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Apps
+
+    'neighborhood.apps.NeighborhoodsConfig',
 ]
 
 MIDDLEWARE = [
